f86204475ced565e7332662855b7b1734cd6c25b
mcadaval <matias.cadaval@gmail.com>
2017-10-05 18:16:54 -0300

    Muevo carpeta data y modifico algunas cosas del main y la clase Package

--- tp/src/main.py
+++ tp/src/main.py
@@ -3,11 +3,31 @@ from scapy.all import *
 
 from model.Package import Package
 
-packets = rdpcap('data/test.pcap')
+packets = rdpcap('../data/DatosEXACTAS-UBA.pcap')
+# packets = rdpcap('../data/DatosLaboratorios-DC.pcap')
 
-list = []
+symbols_occurrences = {}
+total_packets = 0
+
 for pkt in packets:
-    package = Package(pkt)
-    print(package)
-    list.append(package)
+    packet = Package(pkt)
+    symbol = packet.symbol()
+    if symbol in symbols_occurrences:
+        symbols_occurrences[symbol] += 1
+    else:
+        symbols_occurrences[symbol] = 1    
+    total_packets += 1
 
+symbols_probabilities = {}
+for symb, occurrences in symbols_occurrences.items():
+    symbols_probabilities[symb] = occurrences/total_packets
+
+print('Cantidad total de paquetes: ', total_packets)
+
+for symb, occurrences in symbols_occurrences.items():
+    print('symbol: ', symb)
+    print('occurrences: ', occurrences)
+
+for symb, probability in symbols_probabilities.items():
+    print('symbol: ', symb)
+    print('probability: ', probability)
--- tp/src/model/Package.py
+++ tp/src/model/Package.py
@@ -5,40 +5,26 @@ from config import BROADCAST_ADDRESS
 class Package:
     def __init__(self, pkt):
 
-        if IP in pkt or IPv6 in pkt:
-            protocol = "IP"
-            source = pkt['IP'].src
-            destiny = pkt['IP'].dst
+        if pkt.payload:
+            payload = pkt.payload
+            protocol = payload.name
 
-        elif ARP in pkt:
-            protocol = "ARP"
-            source = pkt['ARP'].psrc
-            destiny = pkt['ARP'].pdst
-
-        elif TCP in pkt:
-            protocol = "TCP"
-            source = pkt['TCP'].psrc
-            destiny = pkt['TCP'].pdst
-
-        elif UDP in pkt:
-            protocol = "UDP"
-            source = pkt['UDP'].psrc
-            destiny = pkt['UDP'].pdst
-
+        if pkt.dst == BROADCAST_ADDRESS:
+            destination_type = "BROADCAST"
         else:
-            source = None
-            destiny = None
-            protocol = 'NONE'
-
-        if source == BROADCAST_ADDRESS:
-            destiny_type = "BROADCAST"
-        else:
-            destiny_type = "UNICAST"
+            destination_type = "UNICAST"
 
         self.protocol = protocol
-        self.source = source
-        self.destiny = destiny
-        self.destiny_type = destiny_type
+        self.destination_type = destination_type
+
+    def protocol(self):
+        return self.protocol
+    
+    def destination_type(self):
+        return self.destination_type
+
+    def symbol(self):
+        return (self.destination_type, self.protocol)
 
     def __str__(self):
         return self.protocol
