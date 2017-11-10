e4a378563a0e0854e9fac12e74f16a86aaa7a035
mcadaval <matias.cadaval@gmail.com>
2017-11-10 01:59:57 -0300

    Agrego modo de uso y ayuda al main

--- tp2/src/main.py
+++ tp2/src/main.py
@@ -4,7 +4,19 @@ import sys
 import helpers.Plotter as Plot
 import helpers.Cimbala as cimbala
 
+def printHelp():
+    print("Cantidad inválida de argumentos")
+    print("Modo de uso:")
+    print("   python3", sys.argv[0], "<ip|url> <ttl_maximo>")
+    print("Ejemplo:")
+    print("   python3", sys.argv[0],"dc.uba.ar 20")
+    exit(1)
+
 if __name__ == '__main__':
+        
+    if len(sys.argv) != 3:
+        printHelp();
+        
     dest = sys.argv[1]
     life_span = int(sys.argv[2])
 
